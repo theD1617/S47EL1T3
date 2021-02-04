559688d7cc19e9d59cea40707a6abf34eb3acabe
stereoIII6 <stereoiii6@localhost.localdomain>
2021-02-04 01:20:46 +0100

    lookout

--- src/App.js
+++ src/App.js
@@ -109,22 +109,22 @@ class App extends Component {
 
     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 className="h2">Dashboard</h1>
+        <h1 className="h2">Aktivitäts Cockpit</h1>
         <div className="btn-toolbar mb-2 mb-md-0">
           <div className="btn-group me-2">
-            <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
+            <button type="button" className="btn btn-sm btn-outline-secondary">Teilen</button>
+            <button type="button" className="btn btn-sm btn-outline-secondary">Exportieren</button>
           </div>
           <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-            This week
+            Termine
           </button>
         </div>
       </div>
 
       <canvas className="my-4 w-100 chartjs-render-monitor" id="myChart" width="304" height="128" style={{display: "block", width: "304px", height: "128px" }}></canvas>
 
-      <h2>Section title</h2>
+      <h2>Letzte Käufe</h2>
       <div className="table-responsive">
          <ul className="list-group container-fluid">
           {items.map(item => (
