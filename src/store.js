d3b496e01018e82d883c894d75f7ef829d1a5f99
stereoIII6 <stereoiii6@localhost.localdomain>
2021-02-10 16:14:07 +0100

    mercury

--- src/store.js
+++ src/store.js
@@ -31,7 +31,7 @@ const store = createStore(
     rootReducer, perState, compose(
         applyMiddleware(...middleware),
         // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+        typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
             ? a => a
             : window.__REDUX_DEVTOOLS_EXTENSION__ &&
             window.__REDUX_DEVTOOLS_EXTENSION__()
