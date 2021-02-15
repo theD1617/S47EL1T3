4bb7e98a8112fb370029692d5e46b810de91596a
stereoIII6 <stereoiii6@localhost.localdomain>
2021-02-15 17:09:33 +0100

    neptune

--- src/addons/modules/logReg.js
+++ src/addons/modules/logReg.js
@@ -69,37 +69,51 @@ class LogRegModule extends Component {
 
         return (
             <div>
-                <Form onSubmit={this.onLog} >
+                <div>
+                    <Form onSubmit={this.onLog} >
 
 
-                    <InputGroup>
-                        <Input type="text" placeholder="Benutzername" onChange={this.onChange} id="lnik" name="lnik" />
-                        <Input type="password" placeholder="Passwort" onChange={this.onChange} id="lpin" name="lpin" />
-                    </InputGroup>
-                    <br />
-                    <Button>Einloggen</Button>
-                </Form>
-                <Form onSubmit={this.onReg} >
-                    <br />
+                        <InputGroup>
+                            <Input type="text" placeholder="Nik" onChange={this.onChange} id="lnik" name="lnik" />
+                            <Input type="password" placeholder="Pin" onChange={this.onChange} id="lpin" name="lpin" />
+                        </InputGroup>
+                        <br />
+                        <Button>Einloggen</Button>
+                    </Form><Button>Pin Vergessen ?</Button><Button>Nik Vergessen ?</Button>
+                </div>
+                <div>
+                    <Form onSubmit={this.onReg} >
+                        <br />
             Registrieren<br /><br />
-                    <InputGroup>
-                        <Input type="text" placeholder="Vorname" onChange={this.onChange} id="lname" name="lname" />
-                        <Input type="text" placeholder="Nachname" onChange={this.onChange} id="name" name="name" />
-                    </InputGroup><InputGroup>
-                        <Input type="text" placeholder="Geburtsjahr" onChange={this.onChange} id="age" name="age" />
-                        <Input type="text" placeholder="Land" onChange={this.onChange} id="land" name="land" />
-                    </InputGroup><InputGroup>
-                        <Input type="text" placeholder="Mobil" onChange={this.onChange} id="_mobile" name="_mobile" />
-                    </InputGroup><InputGroup>
-                        <Input type="text" placeholder="Email" onChange={this.onChange} id="_email" name="_email" />
-                    </InputGroup><InputGroup>
-                        <Input type="text" placeholder="Benutzername" onChange={this.onChange} id="nik" name="nik" />
-                        <Input type="text" placeholder="Passwort" onChange={this.onChange} id="pin" name="pin" />
-                    </InputGroup>
-                    <br />
-                    <Button>Neu Anmelden</Button>
+                        <InputGroup>
+                            <Input type="text" placeholder="Vorname" onChange={this.onChange} id="lname" name="lname" />
+                            <Input type="text" placeholder="Nachname" onChange={this.onChange} id="name" name="name" />
+                        </InputGroup><InputGroup>
+                            <Input type="text" placeholder="Geburtsjahr" onChange={this.onChange} id="age" name="age" />
+                            <Input type="text" placeholder="Land" onChange={this.onChange} id="land" name="land" />
+                        </InputGroup><InputGroup>
+                            <Input type="text" placeholder="Mobil" onChange={this.onChange} id="_mobile" name="_mobile" />
+                        </InputGroup><InputGroup>
+                            <Input type="text" placeholder="Email" onChange={this.onChange} id="_email" name="_email" />
+                        </InputGroup><InputGroup>
+                            <Input type="text" placeholder="Benutzername" onChange={this.onChange} id="nik" name="nik" />
+                            <Input type="password" placeholder="Passwort" onChange={this.onChange} id="pin" name="pin" />
+                        </InputGroup>
+                        <br />
+                        <Button>Neu Anmelden</Button>
 
-                </Form>
+                    </Form>
+                </div>
+                <div><br />Aktivierung
+                <br /><br />
+                    <Form onSubmit={this.onAct} >
+                        <InputGroup>
+                            <Input type="text" placeholder="Aktivierungs-Code" onChange={this.onChange} id="act" name="act" />
+                        </InputGroup>
+                        <br />
+                        <Button>Aktivieren</Button>
+                    </Form>
+                </div>
             </div>
         );
     }
--- src/addons/rooms/charts.js
+++ src/addons/rooms/charts.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { Form, Input, Button, InputGroup, Option } from 'reactstrap';
 class Charts extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <canvas className="my-4 w-100 chartjs-render-monitor" id="myChart" width="304" height="128" style={{display: "block", width: "304px", height: "128px" }}></canvas>
-         );
+    state = {}
+    render() {
+        return (
+            <div className="row">
+                <Form onSubmit={this.onAct} >
+                    <InputGroup>
+                        <Input type="text" placeholder="Ihre Suche" onChange={this.onChange} id="seek" name="seek" className="col-6" />
+                        <Button className>Suchen</Button>
+                    </InputGroup>
+                    <InputGroup>
+                        <Input type="select" name="produzent">
+                            <option name="default" value="...">Produzent</option>
+                        </Input>
+                        <Input type="select" name="duftname">
+                            <option name="default" value="...">Duft Name</option>
+                        </Input>
+                        <Input type="select" name="code">
+                            <option name="default" value="...">Duft Code</option>
+                        </Input>
+                        <Input type="select" name="gender">
+                            <option name="default" value="...">Gender</option>
+                        </Input>
+                        <Input type="select" name="year">
+                            <option name="default" value="...">Erscheinungsjahr</option>
+
+                        </Input>
+                        <Button className>Suchen</Button>
+                    </InputGroup>
+
+                </Form>
+            </div>
+        );
     }
 }
- 
+
 export default Charts;
